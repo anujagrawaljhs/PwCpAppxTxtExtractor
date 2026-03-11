226cafeee92229d8dc32db9002616b0b5bbf370d
anujagrawaljhs <121594966+anujagrawaljhs@users.noreply.github.com>
2026-03-12 01:27:37 +0530

    config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-api_id = "24473318"
-api_hash = "e7dd0576c5ac0ff8f90971d6bb04c8f5"
-bot_token = "8034069514:AAHUBpzSCq41jPwsJkDbXuEoVC_yCxzyuw0"
-auth_users = [5840594311]
+api_id = "31364482"
+api_hash = "7a80b0306342c592a1f0e6a08177d312"
+bot_token = "8707410134:AAE1BHnGDca8txU0T-Xn2NrE5mxtlxY0ybI"
+auth_users = [8707410134]
